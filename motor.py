01bb000f0eefafb9d91c50e7fa50b3615082433e
Seungyeop Lee <lsy020206@outlook.kr>
2023-10-17 04:10:25 +0900

    v1

---
+++ motor.py
@@ -0,0 +1,74 @@
+import serial
+import time
+
+class Motor:
+    def __init__ (self, port='COM3', baudrate=9600, checkRange=True):
+        try:
+            self.ser = serial.Serial(port=port, baudrate=baudrate, stopbits=1)
+        except Exception as e:
+            raise Exception(f"Usb not connected or port doesn't have permission for serial. try 'sudo chmod 666 {port}'")
+        
+        # Check for CW and CCW soft limit status
+        cw_soft_limit_status = self.writeAndGetResponse(":CWLSET?")
+        ccw_soft_limit_status = self.writeAndGetResponse(":CCWLSET?")
+        
+        if cw_soft_limit_status == '1':
+            raise Exception("CW Soft Limit is enabled.")
+        if ccw_soft_limit_status == '1':
+            raise Exception("CCW Soft Limit is enabled.")
+        
+        self.setSpeed(400, 400)
+        self.setRate(20, 20)
+        self.goAbs(0, 0)
+        
+        if checkRange:
+            self.goAbs(2000, 2000)
+            self.goAbs(-2000, -2000)
+            self.goAbs(0, 0)
+
+    def writeCommand(self, command: str):
+        command += '\r'
+        self.ser.write(command.encode())
+    
+    def getResponse(self):
+        output = self.ser.read_until(b'\r')
+        return output.decode()[:-1]
+    
+    def writeAndGetResponse(self, command: str):
+        self.writeCommand(command)
+        return self.getResponse()
+    
+    def goAbsWithOutStop(self, xPos, yPos):
+        self.writeCommand(f"axi1:goabs {xPos}")
+        self.writeCommand(f"axi2:goabs {yPos}")
+    
+    def goAbs(self, xPos, yPos):
+        self.writeCommand(f"axi1:goabs {xPos}")
+        self.writeCommand(f"axi2:goabs {yPos}")
+        self.waitForStop()
+        
+    def setSpeed(self, xSpeed, ySpeed):
+        # set L_Speed
+        self.writeCommand(f"axi1:L1 {xSpeed}")
+        # set F_Speed
+        self.writeCommand(f"axi1:F1 {xSpeed}")
+        self.writeCommand(f"axi2:L2 {ySpeed}")
+        self.writeCommand(f"axi2:F2 {ySpeed}")
+        
+    def setRate(self, xRate, yRate):
+        # set Rate
+        self.writeCommand(f"axi1:R1 {xRate}")
+        # set S_Rate
+        self.writeCommand(f"axi1:S1 {xRate}")
+        self.writeCommand(f"axi2:R2 {yRate}")
+        self.writeCommand(f"axi2:S2 {yRate}")
+    
+    def waitForStop(self):
+        while True:
+            if self.writeAndGetResponse("MOTIONAll?") == '0':
+                break
+            time.sleep(0.01)
+        time.sleep(0.1)
+        
+    def close(self):
+        self.ser.close()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Laser-Lithography',
+    version='0.0.1',    
+    description='Python package that controls the DS102 series motor controller.',
+    url='https://gitlab.com/cnu-physics-capstone-design/laser-lithography',
+    author='Seungyeop Lee',
+    author_email='lsy020206@outlook.kr',
+    license='',
+    install_requires=['numpy', 'pyserial', 'tkinter'],
+)
