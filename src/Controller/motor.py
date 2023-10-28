d839c8131884b69a9b311dce7fcabcec2cda7891
Seungyeop Lee <lsy020206@outlook.kr>
2023-10-28 23:19:56 +0900

    Connect motor.py and config.yml

--- src/Controller/motor.py
+++ src/Controller/motor.py
@@ -1,8 +1,18 @@
 import serial
 import time
+import yaml
 
 class Motor:
     def __init__ (self, port='COM3', baudrate=9600, checkRange=True):
+        
+        # Open config
+        with open(file="config.yml", mode="r") as config_file:
+            config = yaml.safe_load(config_file)
+        
+        # Set port and baudrate value with config
+        port = config['Setup']['Port']
+        baudrate = config['Setup']['Baudrate']
+            
         try:
             self.ser = serial.Serial(port=port, baudrate=baudrate, stopbits=1)
         except Exception as e:
@@ -16,22 +26,38 @@ class Motor:
             raise Exception("CW Soft Limit is enabled.")
         if ccw_soft_limit_status == '1':
             raise Exception("CCW Soft Limit is enabled.")
+                        
+        # Set speed value with config
+        x_l_speed = config['X_Axis']['L_Speed']
+        x_f_speed = config['X_Axis']['F_Speed']
+        y_l_speed = config['Y_Axis']['L_Speed']
+        y_f_speed = config['Y_Axis']['F_Speed']
         
-        x_speed = 400
-        y_speed = 400
-        x_rate = 20
-        y_rate = 20
-        init_x_pos = 0
-        init_y_pos = 0
+        # Set rate value with config
+        x_rate = config['X_Axis']['Rate']
+        x_s_rate = config['X_Axis']['S_Rate']
+        y_rate = config['Y_Axis']['Rate']
+        y_s_rate = config['Y_Axis']['S_Rate']
         
-        self.setSpeed(x_speed, y_speed)
-        self.setRate(x_rate, y_rate)
+        # Set initial position value with config
+        init_x_pos = config['Initial_Position']['X']
+        init_y_pos = config['Initial_Position']['Y']
+        
+        # axis setting
+        x = "1"
+        y = "2"
+        
+        self.setSpeed(x, x_l_speed, x_f_speed)
+        self.setSpeed(y, y_l_speed, y_f_speed)
+        self.setRate(x, x_rate, x_s_rate)
+        self.setRate(y, y_rate, y_s_rate)
         self.goAbs(init_x_pos, init_y_pos)
         
         if checkRange:
+            self.goAbs(0, 0)
             self.goAbs(2000, 2000)
             self.goAbs(-2000, -2000)
-            self.goAbs(0, 0)
+            self.goAbs(init_x_pos, init_y_pos)
 
     def writeCommand(self, command: str):
         command += '\r'
@@ -54,21 +80,17 @@ class Motor:
         self.writeCommand(f"axi2:goabs {yPos}")
         self.waitForStop()
         
-    def setSpeed(self, xSpeed, ySpeed):
+    def setSpeed(self, axis, lSpeed, fSpeed):
         # set L_Speed
-        self.writeCommand(f"axi1:L1 {xSpeed}")
+        self.writeCommand(f"axi{axis}:L{axis} {lSpeed}")
         # set F_Speed
-        self.writeCommand(f"axi1:F1 {xSpeed}")
-        self.writeCommand(f"axi2:L2 {ySpeed}")
-        self.writeCommand(f"axi2:F2 {ySpeed}")
+        self.writeCommand(f"axi{axis}:F{axis} {fSpeed}")
         
-    def setRate(self, xRate, yRate):
+    def setRate(self, axis, Rate, sRate):
         # set Rate
-        self.writeCommand(f"axi1:R1 {xRate}")
+        self.writeCommand(f"axi{axis}:R{axis} {Rate}")
         # set S_Rate
-        self.writeCommand(f"axi1:S1 {xRate}")
-        self.writeCommand(f"axi2:R2 {yRate}")
-        self.writeCommand(f"axi2:S2 {yRate}")
+        self.writeCommand(f"axi{axis}:S{axis} {sRate}")
     
     def waitForStop(self):
         while True:
