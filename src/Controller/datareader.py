64103d941a7e5324c06417df21ead56b49e73370
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-08 17:06:35 +0900

    refactor(CSVDataReader): Remove executing code

--- src/Controller/datareader.py
+++ src/Controller/datareader.py
@@ -19,15 +19,3 @@ class CSVDataReader:
     def close(self):
         # 파일을 열었지만 닫지 않는 것이 좋습니다.
         pass
-
-# CSVDataReader 클래스의 인스턴스 생성
-csv_reader = CSVDataReader(path="src\\resources\\test.csv")
-
-# CSV 파일 읽기 메서드 호출
-csv_reader.read_csv()
-
-# X 및 Y 리스트 출력
-print("X:", csv_reader.X)
-print("Y:", csv_reader.Y)
-
-# 파일을 닫을 필요 없음
