ec6b22753ac7df1dd7fe4967dee2fec4fb04298f
Chojeongmin <askme0633@gmail.com>
2023-11-08 20:41:48 +0900

    아두이노

--- src/Controller/sketch_nov8a.ino
+++ src/Controller/sketch_nov8a.ino
@@ -1,21 +1,24 @@
-int data; 
 // 파이썬의 데이터를 저장하기 위함
+int data; 
+
 
 void setup(){
-  Serial.begin(9600);
   // 시리얼 통신 시작, 9600은 통신 속도를 의미함
-  pinMode(13,OUTPUT);
+  Serial.begin(9600);
   // 13번 핀을 출력 부품으로 설정
-  digitalWrite(13,LOW);
+  pinMode(13,OUTPUT);
   // 13번 핀에 'LOW' 신호 전달 
+  digitalWrite(13,LOW);
+
 }
 
 
 void loop() {
+  // 데이터 수신 계속 검사
   while (Serial.available()) {
-    // 데이터 수신 계속 검사
+  // data에 저장
     data=Serial.read();
-    // data에 저장
+  
   }
   
   if (data=='1'){
