c4ab303acb71fc392dbbe00bd6d24a6144c26c61
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-08 17:32:59 +0900

    feat(Recipe): Add method to execute in descending order

--- src/Controller/recipe.py
+++ src/Controller/recipe.py
@@ -1,13 +1,28 @@
 from datareader import CSVDataReader
+from motor import Motor
 
-# CSVDataReader 클래스의 인스턴스 생성
-csv_reader = CSVDataReader(path="src\\resources\\test.csv")
+class Recipe():
+    def __init__(self):
+        self.motor = Motor()
+        
+        # CSVDataReader 클래스의 인스턴스 생성
+        self.csv_reader = CSVDataReader(path="src\\resources\\test.csv")
+        # CSV 파일 읽기 메서드 호출
+        self.csv_reader.read_csv()
+        
+    def goRecipe(self):
+        csv_size = len(self.csv_reader.X)
+        for i in range(csv_size):
+            self.motor.goAbs(self.csv_reader.X[i], self.csv_reader.Y[i])
 
-# CSV 파일 읽기 메서드 호출
-csv_reader.read_csv()
 
-# X 및 Y 리스트 출력
-print("X:", csv_reader.X)
-print("Y:", csv_reader.Y)
+if __name__ == "__main__":
+    recip = Recipe()
 
-# 파일을 닫을 필요 없음
+    # X 및 Y 리스트 출력
+    print("X:", recip.csv_reader.X)
+    print("Y:", recip.csv_reader.Y)
+    # csv_size = len(recip.csv_reader.X)
+    # for i in range(csv_size):
+    #     print(recip.csv_reader.X[i], recip.csv_reader.Y[i])
+
