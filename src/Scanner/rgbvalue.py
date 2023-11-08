dd4ac1d03b0a733f5d63d96a4c2733b72579d3c4
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-08 21:04:13 +0900

    feat: Edit input and output path

--- src/Scanner/rgbvalue.py
+++ src/Scanner/rgbvalue.py
@@ -48,8 +48,8 @@ class ImageProcessor:
                         writer.writerow([row[1], row[0]])
 
 # 이미지 파일 경로와 출력 CSV 파일 이름을 지정합니다.
-image_path = 'C:\\image\\image3.jpg'
-output_csv = 'filtered_pixel_rgb_values.csv'
+image_path = 'resources\\input_image.jpg'
+output_csv = 'resources\\filtered_pixel_rgb_values.csv'
 
 # ImageProcessor 클래스의 인스턴스를 생성하고 이미지 처리를 실행합니다.
 image_processor = ImageProcessor(image_path, output_csv)
