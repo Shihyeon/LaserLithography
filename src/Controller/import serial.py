353999bb91e63ca33d7bf8d994f53425aa287f6a
Chojeongmin <askme0633@gmail.com>
2023-11-11 22:54:53 +0900

    코드 수정

--- src/Controller/import serial.py
+++ src/Controller/import serial.py
@@ -1,7 +1,7 @@
 import serial # 아두이노와 통신을 위해 시리얼 통신 설정
 import time 
 
-arduino = serial.Serial('com9',9600) 
+arduino = serial.Serial('com3',9600) 
 # com9번이 아두이노에 연결, 보드레이트가 9600(1초에 9600bit)
 time.sleep(1)
 
@@ -13,7 +13,7 @@ while 1:
     if (var == '1'): # var에 저장된 값이 1
         var = var.encode ('utf-8')
         # var의 값을 'utf-8' 형식으로 인코드해서 var에 저장
-        arduino.whrite(var)
+        arduino.write(var)
         # 인코드된 'var'의 값이 시리얼 통신을 통해 아두이노 프로그램으로 전송
         print ("LED turned ON")
         time.sleep(1)
@@ -22,4 +22,4 @@ while 1:
         var = var.encode('utf-8')
         arduino.write(var)
         print ("LED tuend OFF")
-        time.slepp(1)
+        time.sleep(1)
