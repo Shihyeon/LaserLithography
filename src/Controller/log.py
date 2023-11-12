8b677862528185e5635029b504c122f1b31dd8c4
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-12 22:30:40 +0900

    feat: Add logging level

--- src/Controller/log.py
+++ src/Controller/log.py
@@ -27,8 +27,10 @@ if __name__ == "__main__":
     logger = logger_instance.get_logger()
     
     # Example usage
+    logger.trace("This is a trace message")
     logger.debug("This is a debug message")
     logger.info("This is an info message")
+    logger.success("This is a success message")
     logger.warning("This is a warning message")
     logger.error("This is an error message")
     logger.critical("This is a critical message")
