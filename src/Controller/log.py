749834598d4ea742c95eb05ef923ce83128369eb
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-12 22:19:59 +0900

    feat: Change package from logging to loguru

--- src/Controller/log.py
+++ src/Controller/log.py
@@ -1,33 +1,29 @@
-import logging
-from logging.handlers import FileHandler
-import datetime
-
-class CustomFileHandler(FileHandler):
-    def __init__(self, filename_prefix, filename_suffix, *args, **kwargs):
-        now = datetime.datetime.now()
-        formatted_datetime = now.strftime("%Y-%m-%d_%H-%M-%S")
-        filename = f"{filename_prefix}_{formatted_datetime}{filename_suffix}"
-        super(CustomFileHandler, self).__init__(filename, *args, **kwargs)
+from loguru import logger
+from datetime import datetime
+import os
 
 class Logger:
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-        self.stream_handler = logging.StreamHandler()
-        self.file_handler = CustomFileHandler("Report", ".log")
-        
-        self.stream_handler.setFormatter(self.formatter)
-        self.file_handler.setFormatter(self.formatter)
-        
-        self.logger.addHandler(self.stream_handler)
-        self.logger.addHandler(self.file_handler)
-        self.logger.setLevel(level=logging.DEBUG)
+    def __init__(self, log_dir):
+        self.log_dir = log_dir
+        self.log_file = self.generate_log_file()
+        self.configure_logger()
+    
+    def generate_log_file(self):
+        now = datetime.now()
+        formatted_datetime = now.strftime("%Y-%m-%d_%H-%M-%S")
+        log_file = os.path.join(self.log_dir, f"server_{formatted_datetime}.log")
+        return log_file
+    
+    def configure_logger(self):
+        logger.remove()  # 기존 핸들러 제거
+        logger.add(self.log_file, level="DEBUG", format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <level>{message}</level>", rotation="1 week", retention="2 weeks")
     
     def get_logger(self):
-        return self.logger
+        return logger
 
 if __name__ == "__main__":
-    logger_instance = Logger()
+    log_dir = "src\\log"  # 로그 파일의 디렉토리 경로
+    logger_instance = Logger(log_dir)
     logger = logger_instance.get_logger()
     
     # Example usage
