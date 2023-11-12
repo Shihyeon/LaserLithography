9644cac0f009f73adfe2b44209dd47d36833f7cd
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-13 01:00:52 +0900

    feat: Add run independently

--- src/scanner/converter.py
+++ src/scanner/converter.py
@@ -47,11 +47,12 @@ class ImageProcessor:
                     if int(row[5]) == 1:
                         writer.writerow([row[1], row[0]])
 
-# 이미지 파일 경로와 출력 CSV 파일 이름을 지정합니다.
-image_path = 'resources\\input_image.jpg'
-output_csv = 'resources\\filtered_pixel_rgb_values.csv'
+if __name__ == "__main__":
+    # 이미지 파일 경로와 출력 CSV 파일 이름을 지정합니다.
+    image_path = 'resources\\input_image.jpg'
+    output_csv = 'resources\\filtered_pixel_rgb_values.csv'
 
-# ImageProcessor 클래스의 인스턴스를 생성하고 이미지 처리를 실행합니다.
-image_processor = ImageProcessor(image_path, output_csv)
-image_processor.process_image()
-image_processor.filter_result()
+    # ImageProcessor 클래스의 인스턴스를 생성하고 이미지 처리를 실행합니다.
+    image_processor = ImageProcessor(image_path, output_csv)
+    image_processor.process_image()
+    image_processor.filter_result()
