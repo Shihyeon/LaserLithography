940c5a5ac298729f04bdc10cac9f829e5eba5b9d
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-13 20:33:56 +0900

    fix

--- src/controller/sketch_nov8a2.ino
+++ src/controller/sketch_nov8a2.ino
@@ -14,17 +14,10 @@ void loop() {
     data = Serial.read();
     
     if (data == '1') {
-      for (i = 0; i < 255; i++) {
-        digitalWrite(9, HIGH);
-        analogWrite(LED, i);
-        delay(10);
-      }
-      for (i = 255; i > 0; i--) {
-        analogWrite(LED, i);
-        delay(10);
-      }
-    } else if (data == '0') {
-      digitalWrite(9, LOW);
+      digitalWrite(9, HIGH); // LED 켜기
+    }
+    else if (data == '0') {
+      digitalWrite(9, LOW); // LED 끄기
     }
   }
 }
