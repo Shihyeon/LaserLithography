d27aad661724141d4e0993e540cac6eacc25c242
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-14 00:58:59 +0900

    refactor(Config): Edit value name

--- src/controller/config.py
+++ src/controller/config.py
@@ -3,4 +3,4 @@ import yaml
 class Config():
     def __init__(self):
         with open(file="src\\config.yml", mode="r") as config_file:
-            self.config = yaml.safe_load(config_file)
+            self.configuration = yaml.safe_load(config_file)
