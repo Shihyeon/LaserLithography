ddcf881e497e35fa426bc76d0d7f5c8ca470283d
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-13 14:46:13 +0900

    refactor(Logger): Edit method name

--- src/controller/log.py
+++ src/controller/log.py
@@ -5,26 +5,26 @@ import os
 class Logger:
     def __init__(self, log_dir="src\\log"):
         self.log_dir = log_dir
-        self.log_file = self.generate_log_file()
-        self.configure_logger()
+        self.log_file = self.generateLogFile()
+        self.configureLogger()
     
-    def generate_log_file(self):
+    def generateLogFile(self):
         now = datetime.now()
         formatted_datetime = now.strftime("%Y-%m-%d_%H-%M-%S")
         log_file = os.path.join(self.log_dir, f"Report_{formatted_datetime}.log")
         return log_file
     
-    def configure_logger(self):
+    def configureLogger(self):
         logger.remove()  # 기존 핸들러 제거
         logger.add(self.log_file, level="TRACE", format="<green>{time:YYYY-MM-DD HH:mm:ss.SS}</green> | <level>{level: <8}</level> | <level>{message}</level>", rotation="1 week", retention="2 weeks")
     
-    def get_logger(self):
+    def getLogger(self):
         return logger
 
 if __name__ == "__main__":
     log_dir = "src\\log"  # 로그 파일의 디렉토리 경로
     logger_instance = Logger(log_dir)
-    logger = logger_instance.get_logger()
+    logger = logger_instance.getLogger()
     
     # Example usage
     logger.trace("This is a trace message")
