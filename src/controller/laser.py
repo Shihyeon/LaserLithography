b1f0e6d6f33be7df7289d5d977ac0952107ad977
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-13 19:52:40 +0900

    fix

--- src/controller/laser.py
+++ src/controller/laser.py
@@ -2,9 +2,10 @@ import serial
 import time
 
 class Laser:
-    def __init__(self, port='com3', baud_rate=9600):
+    def __init__(self, port='COM3', baud_rate=9600):
         self.arduino = serial.Serial(port, baud_rate)
         time.sleep(1)
+        self.var = '0'
 
     def onLaser(self):
         self.var = '1'.encode('utf-8')
@@ -18,17 +19,16 @@ class Laser:
 
     def controlLaser(self):
         print("'1'을 입력하면 Laser ON & '0'을 입력하면 Laser OFF")
-        while True:
-            if self.var == '1':
-                self.turn_on()
-            elif self.var == '0':
-                self.turn_off()
-            else:
-                print("유효한 입력이 아닙니다. '1' 또는 '0'을 입력하세요.")
+        if self.var == '1':
+            self.onLaser()
+        elif self.var == '0':
+            self.offLaser()
+        else:
+            print("유효한 입력이 아닙니다. '1' 또는 '0'을 입력하세요.")
 
 if __name__ == "__main__":
     laser_controller = Laser()
     laser_controller.controlLaser()
     laser_controller.onLaser()
-    time.sleep(5)
+    time.sleep(10)
     laser_controller.offLaser()
