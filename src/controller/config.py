da858d61b82d2f283daec7911ba7b24bb327917a
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-14 21:08:04 +0900

    feat(Config): Add exception handling

--- src/controller/config.py
+++ src/controller/config.py
@@ -2,5 +2,9 @@ import yaml
 
 class Config():
     def __init__(self):
-        with open(file="src\\config.yml", mode="r") as config_file:
-            self.configuration = yaml.safe_load(config_file)
+        try:
+            with open(file="src\\config.yml", mode="r") as config_file:
+                self.configuration = yaml.safe_load(config_file)
+        except FileNotFoundError:
+            print("File not found or path incorrect.")
+            self.configuration = {}
