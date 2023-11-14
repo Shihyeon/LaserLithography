bb3274265189b11788f3c576a4cb6d8833fc30dd
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-15 03:32:43 +0900

    test: Add outline in count label

--- src/test/testwindow.py
+++ src/test/testwindow.py
@@ -5,7 +5,7 @@ class Window:
     def __init__(self, root):
         self.root = root
         self.recipe = Recipe(self)
-        self.count_label = tk.Label(root, text="Count: 0", font=('Arial', 12))
+        self.count_label = tk.Label(root, text="Count: 0", font=('Arial', 12), relief="groove")
         self.count_label.grid(row=2, column=0, columnspan=5)
         self.start_button = tk.Button(root, text="Start", command=self.recipe.startRecipe)
         self.start_button.grid(row=3, column=0, columnspan=2)
