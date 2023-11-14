d7f359f249189077bd04c70cbc114f696c5669df
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-14 21:47:45 +0900

    feat(Window): Change font

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -30,11 +30,11 @@ class Window:
         self.empty2_10.grid(row=2, column=0)
         
         # Go button
-        self.go_abs_button = tk.Button(self.frame2, text="Go Absolute", width=12, font=('Nanumgothic', 11))
+        self.go_abs_button = tk.Button(self.frame2, text="Go Absolute", width=12, font=('Arial', 11))
         self.go_abs_button.grid(row=3, column=0, columnspan=2)
         
         # Stop button
-        self.stop_button = tk.Button(self.frame2, text="Stop", width=12, font=('Nanumgothic', 11))
+        self.stop_button = tk.Button(self.frame2, text="Stop", width=12, font=('Arial', 11))
         self.stop_button.grid(row=3, column=3, columnspan=2)
         
         
@@ -46,7 +46,7 @@ class Window:
         self.empty3_00.grid(row=0)
         
         # Exit button
-        self.exit_button = tk.Button(self.frame3, text="Exit", width=12, font=('Nanumgothic', 11), command=self.closeWindow)
+        self.exit_button = tk.Button(self.frame3, text="Exit", width=12, font=('Arial', 11), command=self.closeWindow)
         self.exit_button.grid(row=1)
         
         self.empty3_20 = tk.Label(self.frame3, height=2)
@@ -58,11 +58,11 @@ class Window:
         self.frame.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan)
         return self.frame
         
-    def setTitle(self, frame, row, column, text, rowspan=1, columnspan=1, width=30, font=('Nanumgothic', 20)):
+    def setTitle(self, frame, row, column, text, rowspan=1, columnspan=1, width=30, font=('Arial', 20)):
         self.title_label = tk.Label(frame, text=text, width=width, font=font)
         self.title_label.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan)
         
-    def setSubTitle(self, frame, row, column, text, rowspan=1, columnspan=1, font=('Nanumgothic', 12)):
+    def setSubTitle(self, frame, row, column, text, rowspan=1, columnspan=1, font=('Arial', 12)):
         self.subtitle_label = tk.Label(frame, text=text, font=font)
         self.subtitle_label.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan)
         
@@ -70,10 +70,10 @@ class Window:
         self.emptybox = tk.Label(frame, width=width, height=height)
         self.emptybox.grid(row=row, column=column)
         
-    def setAxisInputLabel(self, frame, row, column, text, font=('Nanumgothic', 11)):
+    def setAxisInputLabel(self, frame, row, column, text, font=('Arial', 11)):
         self.x_label = tk.Label(frame, text=text, width=2, font=font)
         self.x_label.grid(row=row, column=column)
-        self.x_entry = tk.Entry(self.frame2, width=10, font=('Nanumgothic', 11))
+        self.x_entry = tk.Entry(self.frame2, width=10, font=('Arial', 11))
         self.x_entry.grid(row=row, column=column+1)
         
     def closeWindow(self):
