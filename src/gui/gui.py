6a21d3c51dcadabe8e37c03fa0d8fa33bfd2a8ce
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-15 16:40:08 +0900

    Update gui

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-from controller.recipe import Recipe
+import threading
+import time
+from controller.testrecipe import Recipe
 # from controller.motor import Motor
 
 class Window:
     def __init__(self, root):
         self.root = root
         self.recipe = Recipe(self)
+        self.app = self
 
         self.LargeFrame = tk.Frame(root)
         self.LargeFrame.pack(anchor="center")
@@ -21,47 +24,68 @@ class Window:
         # Frame2: Moving Commands Frame
         self.moving_frame = self.setFrame(self.LargeFrame, 1, 0)
         
-        self.moving_subtitle_00 = self.setSubTitle(self.moving_frame, 0, 0, text="Test moving: Move to Position.", columnspan=5)
+        self.moving_subtitle_00 = self.setSubTitle(self.moving_frame, 0, 0, text="< Test moving: Move to Position >", columnspan=5)
+
+        self.moving_empty_10 = tk.Label(self.moving_frame, height=1)
+        self.moving_empty_10.grid(row=1, column=0)
         
-        self.x_label = self.setAxisInputLabel(self.moving_frame, 1, 0, text="X:")
+        self.x_label = self.setAxisInputLabel(self.moving_frame, 2, 0, text="X:")
         
         self.moving_empty_02 = tk.Label(self.moving_frame, width=2)
-        self.moving_empty_02.grid(row=1, column=2)
+        self.moving_empty_02.grid(row=2, column=2)
         
-        self.y_label = self.setAxisInputLabel(self.moving_frame, 1, 3, text="Y:")
+        self.y_label = self.setAxisInputLabel(self.moving_frame, 2, 3, text="Y:")
         
-        self.moving_empty_10 = tk.Label(self.moving_frame, height=1)
-        self.moving_empty_10.grid(row=2, column=0)
+        self.moving_empty_20 = tk.Label(self.moving_frame, height=1)
+        self.moving_empty_20.grid(row=3, column=0)
         
         self.go_abs_button = tk.Button(self.moving_frame, text="Go Absolute", width=12, font=('Arial', 11), command=self.goAbsButton)
-        self.go_abs_button.grid(row=3, column=0, columnspan=2)
+        self.go_abs_button.grid(row=4, column=0, columnspan=2)
         
         self.stop_button = tk.Button(self.moving_frame, text="Stop", width=12, font=('Arial', 11), command=self.stopAbsButton)
-        self.stop_button.grid(row=3, column=3, columnspan=2)
+        self.stop_button.grid(row=4, column=3, columnspan=2)
 
-        self.moving_empty_40 = self.setEmptyBox(self.moving_frame, 4, 0, height=2)
+        self.moving_empty_40 = self.setEmptyBox(self.moving_frame, 5, 0, height=3)
         
         # Frame3: Recipe operating Frame
         self.recipe_frame = self.setFrame(self.LargeFrame, 3, 0)
 
-        self.recipe_subtitle_00 = self.setSubTitle(self.recipe_frame, 0, 0, text="Recipe operating.", columnspan=5)
+        self.recipe_subtitle_00 = self.setSubTitle(self.recipe_frame, 0, 0, text="< Recipe operating >", columnspan=5)
 
-        self.recipe_count_label = tk.Label(self.recipe_frame, text="-/-", font=('Arial', 12), relief="groove")
-        self.recipe_count_label.grid(row=1, column=0, columnspan=5)
+        self.recipe_empty_10 = tk.Label(self.recipe_frame, height=1)
+        self.recipe_empty_10.grid(row=1, column=0)
+
+        self.scan_count_label = tk.Label(self.recipe_frame, text="- / -", font=('Arial', 12))
+        self.scan_count_label.grid(row=2, column=0, columnspan=5)
+
+        self.run_scan_button = tk.Button(self.recipe_frame, text="Run Scanning", width=12, font=('Arial', 11))
+        self.run_scan_button.grid(row=3, column=0, columnspan=2)
+
+        self.recipe_empty_32 = tk.Label(self.recipe_frame, width=2)
+        self.recipe_empty_32.grid(row=3, column=2)
+
+        self.stop_scan_button = tk.Button(self.recipe_frame, text="Stop Scanning", width=12, font=('Arial', 11))
+        self.stop_scan_button.grid(row=3, column=3, columnspan=2)
+
+        self.recipe_empty_40 = tk.Label(self.recipe_frame, height=1)
+        self.recipe_empty_40.grid(row=4, column=0)
+
+        self.recipe_count_label = tk.Label(self.recipe_frame, text="- / -", font=('Arial', 12))
+        self.recipe_count_label.grid(row=5, column=0, columnspan=5)
 
         self.run_recipe_button = tk.Button(self.recipe_frame, text="Run Recipe", width=12, font=('Arial', 11), command=self.startRecipeButton)
-        self.run_recipe_button.grid(row=2, column=0, columnspan=2)
+        self.run_recipe_button.grid(row=6, column=0, columnspan=2)
 
-        self.recipe_empty_12 = tk.Label(self.recipe_frame, width=2)
-        self.recipe_empty_12.grid(row=2, column=2)
+        self.recipe_empty_62 = tk.Label(self.recipe_frame, width=2)
+        self.recipe_empty_62.grid(row=6, column=2)
 
         self.stop_recipe_button = tk.Button(self.recipe_frame, text="Stop Recipe", width=12, font=('Arial', 11), command=self.stopRecipeButton)
-        self.stop_recipe_button.grid(row=2, column=3, columnspan=2)
+        self.stop_recipe_button.grid(row=6, column=3, columnspan=2)
         
         # Frame4: Exit Frame
         self.exit_frame = self.setFrame(self.LargeFrame, 4, 0)
         
-        self.exit_empty_00 = tk.Label(self.exit_frame, height=2)
+        self.exit_empty_00 = tk.Label(self.exit_frame, height=3)
         self.exit_empty_00.grid(row=0)
         
         self.exit_button = tk.Button(self.exit_frame, text="Exit", width=12, font=('Arial', 11), command=self.closeWindowButton)
@@ -81,7 +105,7 @@ class Window:
         self.title_label.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan)
         
     def setSubTitle(self, frame, row, column, text, rowspan=1, columnspan=1, bg="#E0E0E0", font=('Arial', 12)):
-        self.subtitle_label = tk.Label(frame, text=text, bg=bg, font=font, justify=tk.LEFT)
+        self.subtitle_label = tk.Label(frame, text=text, bg=bg, font=font, justify=tk.LEFT, relief="groove")
         self.subtitle_label.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan, sticky='w')
         
     def setEmptyBox(self, frame, row, column, width=1, height=1):
@@ -102,6 +126,7 @@ class Window:
         self.exit_button.config(state=tk.DISABLED)
         # code (아래에 실행할 코드 작성)
         pass
+        print("On goAbsButto")
         # Enable command (실행이 끝나면 버튼을 다시 활성화 상태로 설정)
         self.go_abs_button.config(state=tk.NORMAL)
         self.run_recipe_button.config(state=tk.NORMAL)
@@ -109,53 +134,90 @@ class Window:
         self.exit_button.config(state=tk.NORMAL)
 
     def stopAbsButton(self):
-        self.go_abs_button.config(state=tk.DISABLED)
-        self.stop_button.config(state=tk.DISABLED)
-        self.run_recipe_button.config(state=tk.DISABLED)
-        self.stop_recipe_button.config(state=tk.DISABLED)
-        self.exit_button.config(state=tk.DISABLED)
+        if self.goAbsButton == True:
+            self.go_abs_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.DISABLED)
+            self.run_recipe_button.config(state=tk.DISABLED)
+            self.stop_recipe_button.config(state=tk.DISABLED)
+            self.exit_button.config(state=tk.DISABLED)
 
-        pass
+            pass
+            print("On stopAbsButton")
 
-        self.go_abs_button.config(state=tk.NORMAL)
-        self.stop_button.config(state=tk.NORMAL)
-        self.run_recipe_button.config(state=tk.NORMAL)
-        self.stop_recipe_button.config(state=tk.NORMAL)
-        self.exit_button.config(state=tk.NORMAL)
+            self.go_abs_button.config(state=tk.NORMAL)
+            self.stop_button.config(state=tk.NORMAL)
+            self.run_recipe_button.config(state=tk.NORMAL)
+            self.stop_recipe_button.config(state=tk.NORMAL)
+            self.exit_button.config(state=tk.NORMAL)
 
+    # startRecipe
     def startRecipeButton(self):
-        self.go_abs_button.config(state=tk.DISABLED)
-        self.stop_button.config(state=tk.DISABLED)
-        self.run_recipe_button.config(state=tk.DISABLED)
-        self.exit_button.config(state=tk.DISABLED)
+        if not self.recipe.isRunning():  # 작업이 실행 중이지 않은 경우에만 실행
+            self.go_abs_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.DISABLED)
+            self.run_scan_button.config(state=tk.DISABLED)
+            self.stop_scan_button.config(state=tk.DISABLED)
+            self.run_recipe_button.config(state=tk.DISABLED)
+            self.exit_button.config(state=tk.DISABLED)
 
-        self.recipe.startRecipe
+            start_thread = threading.Thread(target=self.runRecipeWithButtonControl)
+            start_thread.start()
 
+    def runRecipeWithButtonControl(self):
+        self.recipe.startRecipe()
+        print("On startRecipeButton")
+
+        # Recipe 작업이 완료될 때까지 대기
+        while self.recipe.isRunning():
+            time.sleep(0.1)  # 일시적으로 대기, 이 과정을 수정하여 적절한 대기 방법으로 변경할 수 있습니다.
+
+        # Recipe 작업이 끝나면 버튼 활성화
+        self.app.root.after(0, self.runRecipeEnableButtons)
+
+    def runRecipeEnableButtons(self):
         self.go_abs_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.NORMAL)
+        self.run_scan_button.config(state=tk.NORMAL)
+        self.stop_scan_button.config(state=tk.NORMAL)
         self.run_recipe_button.config(state=tk.NORMAL)
         self.exit_button.config(state=tk.NORMAL)
 
+    # stopRecipe
     def stopRecipeButton(self):
-        self.go_abs_button.config(state=tk.DISABLED)
-        self.stop_button.config(state=tk.DISABLED)
-        self.run_recipe_button.config(state=tk.DISABLED)
-        self.stop_recipe_button.config(state=tk.DISABLED)
-        self.exit_button.config(state=tk.DISABLED)
+        if self.recipe.isRunning():  # 작업이 실행 중인 경우에만 실행
+            self.go_abs_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.DISABLED)
+            self.run_scan_button.config(state=tk.DISABLED)
+            self.stop_scan_button.config(state=tk.DISABLED)
+            self.run_recipe_button.config(state=tk.DISABLED)
+            self.stop_recipe_button.config(state=tk.DISABLED)
+            self.exit_button.config(state=tk.DISABLED)
 
-        self.recipe.stopRecipe
+            self.recipe.stopRecipe()
+            print("On stopRecipeButton")
 
-        self.go_abs_button.config(state=tk.NORMAL)
-        self.stop_button.config(state=tk.NORMAL)
-        self.run_recipe_button.config(state=tk.NORMAL)
-        self.stop_recipe_button.config(state=tk.DISABLED)
-        self.exit_button.config(state=tk.NORMAL)
+            # Recipe 작업이 완료될 때까지 대기
+            while self.recipe.isRunning():
+                time.sleep(0.1)  # 일시적으로 대기, 적절한 대기 방법으로 변경 가능
+
+            self.runRecipeEnableButtons()
+
+            self.go_abs_button.config(state=tk.NORMAL)
+            self.stop_button.config(state=tk.NORMAL)
+            self.run_scan_button.config(state=tk.NORMAL)
+            self.stop_scan_button.config(state=tk.NORMAL)
+            self.run_recipe_button.config(state=tk.NORMAL)
+            self.stop_recipe_button.config(state=tk.NORMAL)
+            self.exit_button.config(state=tk.NORMAL)
 
     def closeWindowButton(self):
         self.root.destroy()
 
     def updateCountLabel(self):
-        self.recipe_count_label.config(text=f"{self.recipe.count}/{self.recipe.csv_size}")
+        count_str = str(self.recipe.count).zfill(len(str(self.recipe.csv_size)))
+        csv_size_str = str(self.recipe.csv_size)
+        formatted_text = f"{count_str} / {csv_size_str}"
+        self.recipe_count_label.config(text=formatted_text, justify='center')
 
 if __name__ == "__main__":
     root = tk.Tk()
