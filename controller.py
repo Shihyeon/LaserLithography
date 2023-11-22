4997252826a0e2393afc9fa986b169b0d4621078
Seungyeop Lee <lsy020206@outlook.kr>
2023-11-22 13:25:16 +0900

    feat: Add ImageConverter class

--- controller.py
+++ controller.py
@@ -206,6 +206,50 @@ class Laser:
     def close(self):
         self.arduino.close()
 
+class ImageConverter:
+    def __init__(self, image_path="resources\\input_image.jpg", output_csv="resources\\filtered_pixel_rgb_values.csv"):
+        self.image_path = image_path
+        self.output_csv = output_csv
+
+    def processImage(self):
+        img = Image.open(self.image_path)
+        img.thumbnail((1000, 1000))
+        transform = tr.Compose([tr.ToTensor()])
+        image_tensor = transform(img)
+        _, height, width = image_tensor.shape
+
+        csv_rgbdata = 'pixel_rgb_values.csv'
+
+        with open(csv_rgbdata, mode='w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["Row", "Column", "Red", "Green", "Blue", "Result"])  # 헤더 작성
+
+            for row in range(height):
+                for col in range(width):
+                    red_value = image_tensor[0, row, col]
+                    green_value = image_tensor[1, row, col]
+                    blue_value = image_tensor[2, row, col]
+                    if red_value <= 0.35 or green_value <= 0.35 or blue_value <= 0.35:
+                        writer.writerow([row, col, red_value.item(), green_value.item(), blue_value.item(), 1])
+                    else:
+                        writer.writerow([row, col, red_value.item(), green_value.item(), blue_value.item(), 0])
+
+    def filterResult(self):
+        csv_rgbdata = 'pixel_rgb_values.csv'
+        csv_filtered = 'filtered_pixel_rgb_values.csv'
+
+        with open(csv_filtered, mode='w', newline='') as filtered_csv_file:
+            writer = csv.writer(filtered_csv_file)
+            writer.writerow(["X", "Y"])  # 헤더 작성
+
+            with open(csv_rgbdata, mode='r') as csv_file:
+                reader = csv.reader(csv_file)
+                next(reader)  # 헤더를 건너뛰기
+
+                for row in reader:
+                    if int(row[5]) == 1:
+                        writer.writerow([row[1], row[0]])
+
 class Recipe():
     def __init__(self, app):
         self.count = 0
