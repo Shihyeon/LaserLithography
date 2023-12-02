79fe1f1a814a7b7e13112740345d6e54faa6d9ba
Seungyeop Lee <lsy020206@outlook.kr>
2023-12-02 14:34:50 +0900

    test(Laser): Update

--- src/controller/laser.py
+++ src/controller/laser.py
@@ -2,17 +2,38 @@ import serial
 import time
 from config import Config
 
+# class Laser:
+#     def __init__(self, port='COM4', baudrate=9600):
+        
+#         configInstance = Config()
+#         config = configInstance.configuration
+
+#         port = config['laser']['setup']['port']
+#         baudrate = config['laser']['setup']['baudrate']
+
+#         try:
+#             self.arduino = serial.Serial(port=port, baudrate=baudrate)
+#         except Exception as e:
+#             raise Exception(f"Usb not connected or port doesn't have permission for serial. try 'sudo chmod 666 {port}'")
+        
+#         time.sleep(2)
+
+#     def onLaser(self):
+#         self.arduino.write(b'1')
+#         print("Laser ON")
+
+#     def offLaser(self):
+#         self.arduino.write(b'0')
+#         print("Laser OFF")
+
+#     def close(self):
+#         self.arduino.close()
+
 class Laser:
     def __init__(self, port='COM4', baudrate=9600):
         
-        configInstance = Config()
-        config = configInstance.configuration
-
-        port = config['laser']['setup']['port']
-        baudrate = config['laser']['setup']['baudrate']
-
         try:
-            self.arduino = serial.Serial(port=port, baudrate=baudrate)
+            self.arduino = serial.Serial(port, baudrate)
         except Exception as e:
             raise Exception(f"Usb not connected or port doesn't have permission for serial. try 'sudo chmod 666 {port}'")
         
@@ -34,7 +55,7 @@ if __name__ == "__main__":
 
     # LED를 켜고 끄는 예시
     laser.onLaser()
-    time.sleep(2)  # LED를 켜두고 2초 대기
+    time.sleep(5)  # LED를 켜두고 2초 대기
     laser.offLaser()
 
     laser.close()
--- src/controller/sketch_nov8a2.ino
+++ src/controller/sketch_nov8a2.ino
@@ -2,6 +2,7 @@ const int ledPin = 9;  // 9번 핀을 사용
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
   Serial.begin(9600);
 }
 
@@ -10,10 +11,10 @@ void loop() {
     char command = Serial.read();
     if (command == '1') {
       digitalWrite(ledPin, HIGH);  // LED를 켭니다.
-      Serial.println("LED ON");
+      Serial.println("Laser ON");
     } else if (command == '0') {
       digitalWrite(ledPin, LOW);   // LED를 끕니다.
-      Serial.println("LED OFF");
+      Serial.println("Laser OFF");
     }
   }
 }
